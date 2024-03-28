0abb051e3f271537cd0a5fc45f7207f738848536
shinta <flekystyley@gmail.com>
2024-03-28 16:37:41 +0900

    add comment

--- users/snt/vim-config.nix
+++ users/snt/vim-config.nix
@@ -60,6 +60,8 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
+---------------------------------------------------------------------
+-- Add our go conf
 local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
