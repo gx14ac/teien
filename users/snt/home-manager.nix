afa6479c5892b966777d2825b2b04d1dedc69bbf
shinta@runetale.com <shinta@gx14ac.com>
2026-02-11 16:07:10 +0900

    applied policy

--- users/snt/home-manager.nix
+++ users/snt/home-manager.nix
@@ -114,6 +114,7 @@ in {
       "source ${pkgs.theme-bobthefish}/functions/fish_prompt.fish"
       "source ${pkgs.theme-bobthefish}/functions/fish_right_prompt.fish"
       "source ${pkgs.theme-bobthefish}/functions/fish_title.fish"
+      (builtins.readFile ./config.fish)
       "set -g SHELL ${pkgs.fish}/bin/fish"
     ]);
 
